f6d55f6b986a947e5188672e08948fcbe83b65d7
santwoxx <brisasofc@gmail.com>
2026-08-14 11:45:05 -0300

    fix: add COOP headers to allow firebase google signin popups

--- next.config.js
+++ next.config.js
@@ -8,6 +8,19 @@ const nextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
